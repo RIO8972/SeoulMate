d43cd2bc456517a9233bf870e3d772edb8554b57
ksw <ksw89726756@gmail.com>
2025-03-25 01:13:48 +0900

    feat: add testcontroller

---
+++ back/oauthlogin/src/main/java/com/knu/oauthlogin/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.knu.oauthlogin.controller;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class TestController {
+    @GetMapping("/controller")
+    public String test() {
+        log.info("dd");
+        return "test";
+    }
+}
