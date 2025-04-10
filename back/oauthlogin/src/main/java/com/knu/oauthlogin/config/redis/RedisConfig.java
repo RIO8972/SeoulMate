a2ddf52fa0d3579945c6734878784fc1725ec523
ksw <ksw89726756@gmail.com>
2025-04-11 03:03:46 +0900

    feat: 로그인 추가

--- back/oauthlogin/src/main/java/com/knu/oauthlogin/config/redis/RedisConfig.java
+++ back/oauthlogin/src/main/java/com/knu/oauthlogin/config/redis/RedisConfig.java
@@ -1,11 +1,7 @@
 package com.knu.oauthlogin.config.redis;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
-@Configuration
+
 public class RedisConfig {
     /*
     @Bean
--- back/oauthlogin/src/main/java/com/knu/oauthlogin/config/redis/RedisHealthCheck.java
+++ back/oauthlogin/src/main/java/com/knu/oauthlogin/config/redis/RedisHealthCheck.java
@@ -1,12 +1,5 @@
 package com.knu.oauthlogin.config.redis;
 
-import lombok.RequiredArgsConstructor;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
 public class RedisHealthCheck  {
 
 }
