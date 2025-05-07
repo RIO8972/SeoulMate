93a6db127051bd8c91a596eb7ca0f6a831a2a7ea
ksw <ksw89726756@gmail.com>
2025-05-07 15:30:00 +0900

    feat: 도시 api

--- back/cityapi/src/main/java/com/knu/cityapi/cityapi/service/seoul/SeoulRegionService.java
+++ back/cityapi/src/main/java/com/knu/cityapi/cityapi/service/seoul/SeoulRegionService.java
@@ -1,10 +1,17 @@
 package com.knu.cityapi.cityapi.service.seoul;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +21,7 @@ public class SeoulRegionService {
     @Value("${seoul.api.key}")
     private String seoulApiKey;
 
-    public Mono<String> searchRegion(String region){
+    public Mono<JsonNode> searchRegion(String region){
         //log.info("region : "+ region);
         //log.info("seoulApiKey :"+ seoulApiKey);
         return webClient.get()
@@ -22,6 +29,17 @@ public class SeoulRegionService {
                         .path("/{apiKey}/json/citydata_ppltn/1/5/{region}")
                         .build(seoulApiKey, region))
                 .retrieve()
-                .bodyToMono(String.class); // 필요에 따라 DTO로 바꿔도 됨
+                .bodyToMono(JsonNode.class); // 필요에 따라 DTO로 바꿔도 됨
+    }
+    public Mono<Map<String, JsonNode>> searchRegionsAsMap(List<String> regions) {
+        return Flux.fromIterable(regions)
+                // 각 region마다 Mono<String> 요청을 맵핑
+                .flatMap(region ->
+                        searchRegion(region)
+                                // 응답값에 region 키를 함께 묶어서 Tuple2로 방출
+                                .map(result -> Tuples.of(region, result))
+                )
+                // Tuple2(region, result)들을 Map으로 수집
+                .collectMap(Tuple2::getT1, Tuple2::getT2);
     }
 }
